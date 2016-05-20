6d1b39acbc439ab38e6a9e18603b5303bcf5d07b
Decors <decors1215@gmail.com>
2016-05-20 12:59:12 +0900

    Add `-l` option to `set`

--- functions/man.fish
+++ functions/man.fish
@@ -1,12 +1,12 @@
 function man --description 'Format and display manual pages'
     set -lx MANPATH $__fish_datadir/man $MANPATH ""
 
-    set -q man_blink; and set blink (set_color $man_blink); or set blink (set_color -o red)
-    set -q man_bold; and set bold (set_color $man_bold); or set bold (set_color -o 5fafd7)
-    set -q man_standout; and set standout (set_color $man_standout); or set standout (set_color 949494)
-    set -q man_underline; and set underline (set_color $man_underline); or set underline (set_color -u afafd7)
+    set -q man_blink; and set -l blink (set_color $man_blink); or set -l blink (set_color -o red)
+    set -q man_bold; and set -l bold (set_color $man_bold); or set -l bold (set_color -o 5fafd7)
+    set -q man_standout; and set -l standout (set_color $man_standout); or set -l standout (set_color 949494)
+    set -q man_underline; and set -l underline (set_color $man_underline); or set -l underline (set_color -u afafd7)
 
-    set end (printf "\e[0m")
+    set -l end (printf "\e[0m")
 
     set -lx LESS_TERMCAP_mb $blink
     set -lx LESS_TERMCAP_md $bold
